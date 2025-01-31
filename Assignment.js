fd3931d1bacc14ff33a7d3781853c857b93be19a
hunain shahid <hunainshahid032@gmail.com>
2025-02-01 04:01:26 +0500

    second js Assignment

---
+++ Assignment.js
@@ -0,0 +1,16 @@
+// Multiplication Tables
+let getNumber = prompt('Enter Your Number');
+for (let i = 1; i <=10; i++) {
+   console.log(`${getNumber} x ${i} = ${getNumber*i}`)
+}
+// Email Address
+let getName = prompt('Enter Your Name');
+let randomNumber = Math.floor(Math.random() * 900)+100;
+console.log(`${getName}${randomNumber}@gmail.com`)
+// Name in uppercase or calculator a lenght of name 
+let userName = prompt('Whats Your Name');
+let nameLenght = '';
+for(let i=0; i <= userName.length; i++){
+    nameLenght = [i]
+}
+console.log(`${userName.toUpperCase()} and Letter have ${nameLenght}`);
